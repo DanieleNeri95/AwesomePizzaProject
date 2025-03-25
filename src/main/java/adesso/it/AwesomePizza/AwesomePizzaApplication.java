c71c3a331616696e3ca4a7f9faf96a189136f7c1
DanieleNeri <danielenerichieti@gmail.com>
2025-03-25 23:04:02 +0100

    Allineamento versioni POM + Swagger

--- src/main/java/adesso/it/AwesomePizza/AwesomePizzaApplication.java
+++ src/main/java/adesso/it/AwesomePizza/AwesomePizzaApplication.java
@@ -1,9 +1,18 @@
 package adesso.it.AwesomePizza;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "AwesomePizza API",
+				version = "1.0",
+				description = "API per la gestione di un servizio di pizze"
+		)
+)
 public class AwesomePizzaApplication {
 
 	public static void main(String[] args) {
